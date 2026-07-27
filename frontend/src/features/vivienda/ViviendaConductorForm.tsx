5bd50f6915b64a066f20d07ea009ddb635ff7c92
valentinovenier <vale07venier@gmail.com>
2026-07-27 17:16:25 -0300

    cambios

--- frontend/src/features/vivienda/ViviendaConductorForm.tsx
+++ frontend/src/features/vivienda/ViviendaConductorForm.tsx
@@ -41,6 +41,7 @@ export const ViviendaConductorForm = ({ label, conductor, onChange, tramoId, hid
                          calculated.resultadoCalculo.pasosVerificacion.length > 0;
 
         if (esValido && JSON.stringify(calculated.resultadoCalculo) !== JSON.stringify(conductor.resultadoCalculo)) {
+            console.log('Depuración ViviendaConductorForm - onChange:', calculated);
             onChange(calculated);
         }
     }
